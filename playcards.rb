1fbd1148ec04a950861458a8e7ce04ad21a54d30
Syuji <commentonly.is@gmail.com>
2016-11-26 17:09:51 +0900

    json形式で現在の山札を返す機能と、配列を渡す事で既存の山札の並びを再現する機能を実装しました。

--- playcards.rb
+++ playcards.rb
@@ -1,11 +1,32 @@
 # coding: utf-8
+require 'json'
 
 #トランプの山を表すクラス
 class Playcards
-  def initialize
-    @cardarray = (0..52).to_a.shuffle
-    @pointer = 0
+  def initialize(initobj = nil, initpointer = 0)
+    if initobj == nil || !checkconsistency(initobj) then
+      @cardarray = (0..52).to_a.shuffle
+    else
+      @cardarray = initobj
+    end
+    @pointer = initpointer
     @carddef = getcarddef()
+  end
+
+  #山札配列の整合性を調べる
+  def checkconsistency(cardarr)
+    if cardarr.length != 53 then
+      return false
+    end
+
+    sortedarr = cardarr.sort
+
+    sortedarr.each_with_index do |item, index|
+      if item != index then
+        return false
+      end
+    end
+    return true
   end
 
   #カードの定義配列を作って返す
@@ -51,6 +72,11 @@ class Playcards
   def remaining
     return @cardarray.length - @pointer
   end
+
+  #山札の情報をjsonで取得する
+  def getjson
+    JSON::dump(@cardarray)
+  end
 end
 
 if __FILE__ == $0
@@ -71,4 +97,12 @@ if __FILE__ == $0
     puts cards.getcardinfo(card)
     puts cards.remaining
   end
+
+  #山札の並びを得る
+  puts cards.getjson()
+
+  #既存の配列で初期化
+  objarr = [25,45,27,15,39,49,32,14,48,0,2,12,30,23,52,40,36,26,28,33,41,13,11,1,37,8,22,47,20,10,7,51,9,21,3,29,6,42,24,16,50,19,4,18,35,31,44,17,43,34,46,38,5]
+  altcards = Playcards.new(objarr)
+  puts altcards.getcardinfo(altcards.draw)
 end
